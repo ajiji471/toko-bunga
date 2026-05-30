123c256eb388b30227c5c3f8c9b0f3e52a27f34c
Ajiji <ijiajiji@gmail.com>
2026-05-30 16:34:25 +0700

    add route akses

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// rute akses produk (resource controller)
+Route::resource('products', ProductController::class);
